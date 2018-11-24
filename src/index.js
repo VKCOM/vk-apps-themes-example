7a1e3b7670c7ed2609fa3bc867b4c9c39c347e12
Anton Tsivarev <a.tsivarev@corp.vk.com>
2018-11-24 15:44:31 +0200

    MVP

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import connect from '@vkontakte/vkui-connect';
 import App from './App';
-import registerServiceWorker from './sw';
 
 // Init VK App
 connect.send('VKWebAppInit', {});
 
-// Service Worker For Cache
-registerServiceWorker();
+connect.subscribe((e) => {
+    switch (e.detail.type) {
+        case 'VKWebAppUpdateConfig':
+            let schemeAttribute = document.createAttribute('scheme');
+            schemeAttribute.value = e.detail.data.scheme ? e.detail.data.scheme : 'client_light';
+            document.body.attributes.setNamedItem(schemeAttribute);
+            break;
+
+        default:
+            console.log(e.detail.type);
+    }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
